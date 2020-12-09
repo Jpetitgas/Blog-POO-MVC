3812d75da78bb6311a40520639f156f999f64370
Jerome Petitgas <jpetitgas63@gmail.com>
2020-12-09 10:03:04 +0100

    correction to Codacy

--- public/assets/js/band.js
+++ public/assets/js/band.js
@@ -1,23 +1,23 @@
-var get = getCookie('acceptCookies');
+var get = getCookie("acceptCookies");
 if (get !== "true") {
-   document.getElementById('cookie').style.visibility="visible";  
+   document.getElementById("cookie").style.visibility="visible";  
 }
-var bottom=document.getElementById('valided');
+var bottom=document.getElementById("valided");
 
 bottom.onclick= setCookie;
 
 function setCookie()
 {
-    document.getElementById('cookie').style.visibility = "hidden";
+    document.getElementById("cookie").style.visibility = "hidden";
     document.cookie="acceptCookies=true";
 }
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
+    var ca = decodedCookie.split(";");
     for(var i = 0; i <ca.length; i++) {
       var c = ca[i];
-      while (c.charAt(0) == ' ') {
+      while (c.charAt(0) == " ") {
         c = c.substring(1);
       }
       if (c.indexOf(name) == 0) {
