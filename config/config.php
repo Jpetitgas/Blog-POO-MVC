3c43d8529162d5b165690bfb008eb44949eb7baf
Jerome Petitgas <jpetitgas63@gmail.com>
2020-12-19 10:59:32 +0100

    modified config file

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 const MAIL="yourmail@mail.com";
-const BASE_PATH = "http://blog/";
-const DB_DSN = 'mysql:dbname=blog;host=localhost';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = '';
+const BASE_PATH = "http://****/";
+const DB_DSN = 'mysql:dbname=****;host=*****';
+const DB_USERNAME = '***';
+const DB_PASSWORD = '***';
